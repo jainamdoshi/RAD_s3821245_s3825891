a659929c39c48f51dd3c73087fb473b5efc3e24b
Ubuntu <ubuntu@ip-172-31-29-58.ec2.internal>
2021-05-07 20:45:34 +0000

    Banner show more poplular item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-    def isProductInSavedlist(product)
-      savedlist = Savedlist.find(@currentUserSavedlistID)
-      return savedlist.products.find_by(id: product.id) ? true : false
-    end
 end
