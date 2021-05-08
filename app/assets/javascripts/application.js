1979cf2dd5d4e30c92686ac50a1c8be131251312
Ubuntu <ubuntu@ip-172-31-30-241.ec2.internal>
2021-05-08 13:53:34 +0000

    Added Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
