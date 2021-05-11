0a8349dc35977c8eb5310d6bf566b18123fc305b
Ubuntu <ubuntu@ip-172-31-29-58.ec2.internal>
2021-05-11 11:16:50 +0000

    Collection's card in home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ Product.destroy_all
 product1 = Product.create({:name=>"Roadster", :price => 20, :score=>5, :photoURL=>"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10940528/2020/2/13/3e484090-d487-4252-9f30-beab10ce1f0a1581594832846-Roadster-Men-Tshirts-4501581594830605-1.jpg"})
 product2 = Product.create({:name=>"Mast & Harbour", :price => 40, :score=>8, :photoURL=>"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11607366/2020/7/7/0934b1f4-e34e-4201-a523-f21ae3a3987c1594121242660-Mast--Harbour-Men-Green-Solid-Polo-Collar-T-shirt-5641594121-1.jpg"})
 product3 = Product.create({:name=>"Akatsuki Cosplay Hoodie", :price=>59.99, :score=>20, :photoURL=>"https://images-na.ssl-images-amazon.com/images/I/41cUihzGv-L._AC_SY780_.jpg"})
-product4 = Product.create({:name=>"Black Polarised Sunglasses", :price=>32.45, :score=>8, :photoURL=>"https://static.fibre2fashion.com/articleresources/images/48/4785/brief-history-of-sunglasses.jpg"})
+product4 = Product.create({:name=>"Black Polarised Sunglasses", :price=>32.45, :score=>8, :photoURL=>"https://i1.adis.ws/i/tom_ford/FT0711_01A_APPENDGRID?$listing_grid$"})
 
 puts "Total number of products added: #{Product.all.count}"
 puts "Product names: #{Product.all.pluck("name")}"
