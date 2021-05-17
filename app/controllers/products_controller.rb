24353b1f6a68a2285d91f5445dcc9e6805cfa3f9
Ubuntu <ubuntu@ip-172-31-29-58.ec2.internal>
2021-05-17 06:09:47 +0000

    product details

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,13 +18,46 @@ class ProductsController < ApplicationController
   
   def show
     @product = Product.find(params[:id])
-    @stock = @product.stocks.first 
-    puts "-------------------------------------------------------------------------------- #{params[:size]}"
-    @stockQuantity = Stock.find_by(stock_quantity_params).quantity
+    
+    # puts params
+    # puts findFilter
+    
+    
+    # if findFilter
+    #   @stockQuantity = Stock.find_by(product_id: @product.id, colour: params[:colour], size: params[:size]).quantity
+    #   redirect_to product_path(@product)
+    # else
+    #   @stockQuantity = 0
+    # end
+    #   puts @stockQuantity
+    
+    if (params[:size])
+      @stockQuantity = Stock.find_by(product_id: @product.id, colour: params[:colour], size: params[:size]).quantity
+      # redirect_to product_path(@product)
+    else
+      @stockQuantity = @product.stocks.first.quantity
+    end
+    puts "-------------------------------------------------------------------------------- #{params}"
   end
   
+  # def quan
+  #   puts "Jainam"
+  #   @stockQuantity = Stock.find_by(product_id: 3, colour: params[:colour], size: params[:size]).quantity
+  #   redirect_to product_path(:id=>3)
+  # end
+  
   private
+  
+  # def findFilter
+  #   if (@stockQuantity != 0 params[:size] && )
+  #     @size = params[:size]
+  #     @colour = params[:colour]
+  #     return true;
+  #   end
+  #     return false;
+  # end
+  
   def stock_quantity_params
-    params.require(:product).permit(:size, :colour)
+    params.require(:product).permit(:product_id, :size, :colour)
   end
 end
