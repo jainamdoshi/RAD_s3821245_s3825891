e19a49abb5daa459794771e3d03b5bcd368b8b8a
Ubuntu <ubuntu@ip-172-31-29-58.ec2.internal>
2021-05-23 22:07:47 +0000

    Rating files

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,23 @@
+class RatingsController < ApplicationController
+  
+   before_action :authorization, only: [:index, :create]
+  
+  def index
+    rating = Rating.find_by(user_id: current_user.id)
+    if (rating)
+      redirect_to root_path
+    end
+  end
+  
+  def create
+    Rating.create(user_id: current_user.id, rate: params[:rate])
+    redirect_to root_path, success: "Thank you for your feedback!"
+  end
+  
+  private
+    def authorization
+        if session[:user_id].blank?
+            redirect_to root_path
+        end
+    end
+end
